9f98359567f2c77e7da9f317f56c12ce333c9130
Odhi Ahmad <odhiahmad15@gmail.com>
2023-10-20 14:46:08 +0700

    :wrench: update repo

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 import { Navigate } from "react-router-dom";
-
-
 
 const PrivateRoute = ({ isAuthenticated, children }) => {
   return isAuthenticated ? children : <Navigate to="/login" />;
